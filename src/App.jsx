e3b031dc8431715ace8f14b8cb4acdb1e913db88
sarthak pokhrel <sarthakpokhrel@sarthaks-MacBook-Air-9.local>
2025-11-20 20:12:33 -0500

    Update for the Demo

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React Hello.   http://testsarthak.duckdns.org/</h1>
+      <h1>Vite + React Hello. Its 8:10  http://testsarthak.duckdns.org/</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
